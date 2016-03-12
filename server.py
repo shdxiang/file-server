4ebce3286d17e872a2d089fe1761c736b362d80f
root <root@asz-test-2.(none)>
2016-03-12 13:05:11 +0800

    Add loop read write

--- server.py
+++ server.py
@@ -22,22 +22,22 @@ class Servers(SRH):
 
     def handle_upload(self):
         #data = self.request.recv(2)
-        data = self.rfile.read(2)
+        data = self.loop_read(2)
         print len(data)
         name_len, = struct.unpack('>H', data)
         logger.debug('name len: %d', name_len)
 
         #name = self.request.recv(name_len)
-        name = self.rfile.read(name_len)
+        name = self.loop_read(name_len)
         logger.debug('name: %s', name)
 
         #data = self.request.recv(4)
-        data = self.rfile.read(4)
+        data = self.loop_read(4)
         content_len, = struct.unpack('>I', data)
         logger.debug('content len: %d', content_len)
 
         #content = self.request.recv(content_len)
-        content = self.rfile.read(content_len)
+        content = self.loop_read(content_len)
         print 'read len: ', len(content)
 
         path = '/tmp/' + name
@@ -46,9 +46,10 @@ class Servers(SRH):
         fo.close()
 
         logger.debug("send ack")
-        data = struct.pack('>H', 0x03)
+        data = struct.pack('>HH', 0x03, name_len)
+        data += name
         #self.request.send(data)
-        self.wfile.write(data)
+        self.loop_write(data)
 
     def handle_download(self):
         print len(data)
@@ -56,7 +57,7 @@ class Servers(SRH):
         logger.debug('name len: %d', name_len)
 
         #name = self.request.recv(name_len)
-        name = self.rfile.read(name_len)
+        name = self.loop_read(name_len)
         logger.debug('name: %s', name)
         path = '/tmp/' + name
         fo = open(path, 'wb')
@@ -67,21 +68,35 @@ class Servers(SRH):
         data = struct.pack('>I', content_len)
         data += content
 
+        self.loop_write(data)
+
+    def loop_read(self, size):
+        data = ''
+        while (len(data) < size):
+            d = self.rfile.read(size - len(data))
+            data += d
+        return data
+
+    def loop_write(self, data):
         self.wfile.write(data)
 
     def handle(self):
         logger.debug('got connection from: %s' ,self.client_address)
         while True:
             try:
-                data = self.rfile.read(2)
+                data = self.loop_read(2)
                 print len(data)
                 msg_type, = struct.unpack('>H', data)
                 print 'msg type:', msg_type
 
                 if msg_type == 1:
+                    logger.debug('upload start')
                     self.handle_upload()
+                    logger.debug('upload finished')
                 elif msg_type == 2:
+                    logger.debug('download start')
                     self.handle_download()
+                    logger.debug('download finished')
 
             except Exception as e:
                 logger.debug(e)
